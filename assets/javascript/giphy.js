6606fbceff03a1fb589df9353bdbe52695ae9f73
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-03-14 20:09:36 -0600

    gifs now add on click

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,12 +1,11 @@
 
-// 
+
 var topics = ["motocross", "utah jazz", "conor mcgregor", "donovan mitchell", "supercross",
-            "golf", "cody garbrandt", "ken roczen", "dean wilson", "tesla", "apple", "nike",
-            "ufc", "ricky rubio", "bahamas"];
+            "golf", "cody garbrandt", "tesla", "apple", "nike", "ufc", "ricky rubio", "bahamas"];
 
 
 
-// function that will take our topics and create a button for each
+// function that will take our topics array and create a button for each
 function topicButtons() {
 
     $("#topic-buttons").empty();
@@ -17,10 +16,29 @@ function topicButtons() {
         button.addClass("topics");
         button.attr("data-name", topics[i]);
         button.text(topics[i]);
-
+        
         $("#topic-buttons").append(button);
-
     }
 }
-
 topicButtons()
+
+// create a button that gets giffs based on the button value
+$("#topic-buttons").on("click", ".topics" ,function() {
+    
+    var userClick = $(this).attr("data-name")
+                                                                                                        // add limit
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userClick + "&api_key=a5ddXmi4hflpwdykgf8ozFMiM45YCFb1&limit=10";
+    console.log(queryURL); 
+ 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        
+        // loop that will loop through the response image and display 10 on button click
+        for (var i = 0; i < response.data.length; i++) {
+            var img = $("<img src="+response.data[i].images.fixed_width.url+">");
+            $("#gif-view").append(img);
+        }
+    });
+});
